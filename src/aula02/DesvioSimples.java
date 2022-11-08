3698f5edc2055d3d02999d7ee6769cba65f0391f
andresoares37 <andresoares37@gmail.com>
2022-11-08 20:41:06 -0300

    Arquivos Aula 02

--- src/aula02/DesvioSimples.java
+++ src/aula02/DesvioSimples.java
@@ -1,10 +1,22 @@
 package aula02;
 
+import java.util.Scanner;
+
 public class DesvioSimples {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner leitor = new Scanner(System.in);
+		
+		System.out.println("Informe sua idade:");
+		int idade = leitor.nextInt();
+		leitor.close();
+		if (idade < 12) {
+			System.out.println("Vc é um bosta!");
+		}
+		else {
+			System.out.println("Vc é legal!");
+		}
+		System.out.print("Sua idade é: ");		
 	}
 
 }
